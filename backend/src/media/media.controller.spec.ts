bf33c25d6ce954b8e457f9bdd619aa95d445eda0
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-06-06 19:46:02 +0300

    added additional tests for media controller

--- backend/src/media/media.controller.spec.ts
+++ backend/src/media/media.controller.spec.ts
@@ -4,6 +4,7 @@ import { env } from 'process';
 import { MediaController } from './media.controller';
 import { MediaService } from './media.service';
 import { enums } from '../common';
+import { BadRequestException } from '@nestjs/common';
 
 describe('MediaController', () => {
   let controller: MediaController;
@@ -37,5 +38,86 @@ describe('MediaController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
     expect(service).toBeDefined();
+  });
+});
+
+describe('send valid url', () => {
+  let controller: MediaController;
+  let service: MediaService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ClientsModule.register([
+          {
+            name: 'OCR_SERVICE',
+            transport: Transport.RMQ,
+            options: {
+              urls: [`amqp://rabbitmq:${env.RMQ_PORT}`],
+              queue: enums.Queues.url,
+              queueOptions: {
+                durable: false,
+              },
+            },
+          },
+        ]),
+      ],
+      controllers: [MediaController],
+      providers: [MediaService],
+    }).compile();
+
+    controller = module.get<MediaController>(MediaController);
+    service = module.get<MediaService>(MediaService);
+  });
+
+  it('should return the success sending message', async () => {
+    const id = '123xyz';
+    const fileUrl = 'https://www.pixabay.com/asdsadds.jpg';
+    const result = `File url was successfully send for processing with request id=${id}`;
+    const res = {
+      statusCode: 200,
+      status: 'success',
+      message: result,
+    };
+
+    jest.spyOn(service, 'sendToQueue').mockImplementation(async () => result);
+
+    expect(await controller.sendLinkToQueue({ fileUrl })).toStrictEqual(res);
+  });
+});
+
+describe('send invalid url (invalid file type or format)', () => {
+  let controller: MediaController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ClientsModule.register([
+          {
+            name: 'OCR_SERVICE',
+            transport: Transport.RMQ,
+            options: {
+              urls: [`amqp://rabbitmq:${env.RMQ_PORT}`],
+              queue: enums.Queues.url,
+              queueOptions: {
+                durable: false,
+              },
+            },
+          },
+        ]),
+      ],
+      controllers: [MediaController],
+      providers: [MediaService],
+    }).compile();
+
+    controller = module.get<MediaController>(MediaController);
+  });
+
+  it('should return the Bad request', async () => {
+    const fileUrl = 'https://www.pixabay.com/asdsadds.mp3';
+
+    await expect(controller.sendLinkToQueue({ fileUrl })).rejects.toThrowError(
+      BadRequestException,
+    );
   });
 });
